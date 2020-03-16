88756502ec13882197fe5e1a66f301a440d4b7e1
Giovanni Lucas <giovannilucasmoura@gmail.com>
2020-03-15 22:27:13 -0300

    Refatoração do código e argumentos para uso

--- GatoCachorro.py
+++ GatoCachorro.py
@@ -4,11 +4,6 @@ import numpy as np
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils, visualize
 
-caminho_base = os.path.abspath(os.getcwd())
-caminho_Logs = os.path.join(caminho_base, "logs")
-caminho_pesos_coco = os.path.join(caminho_base, 'mask_rcnn_coco.h5')
-arquivo_imagem = 'imagem.jpg'
-
 class InferenceConfig(Config):
     NAME = 'COCO'
     GPU_COUNT = 1
@@ -16,30 +11,47 @@ class InferenceConfig(Config):
     DETECTION_MIN_CONFIDENCE = 0.9
     NUM_CLASSES = 1 + 80
 
+def detectar_classes(modelo, arquivo_imagem):    
+    imagem = skimage.io.imread(arquivo_imagem)
+    r = modelo.detect([imagem], verbose = 0)[0]
+    
+    for classe in r['class_ids']:
+        if((classe != 16) and (classe != 17)):
+            i = np.where(r['class_ids'] == classe)[0]
+            r['class_ids'] = np.delete(r['class_ids'], i, 0)
+            r['masks'] = np.delete(r['masks'], i, 2)
+            r['rois'] = np.delete(r['rois'], i, 0)
+            r['scores'] = np.delete(r['scores'], i, 0)
+    
+    for i in range(len(r['class_ids'])):
+        r['class_ids'][i] -= 15
+    
+    nome_arquivo = '.'.join(arquivo_imagem.split('.')[:-1])
+    visualize.display_instances(imagem, r['rois'], r['masks'], r['class_ids'], 
+                                ['BG', 'Gato', 'Cachorro'], r['scores'],
+                                nome_arquivo = nome_arquivo)
+    
+    print("Arquivo salvo em " + nome_arquivo + "_detectado.png")
+
+caminho_base = os.path.abspath(os.getcwd())
+caminho_Logs = os.path.join(caminho_base, "logs")
+caminho_pesos = os.path.join(caminho_base, 'mask_rcnn_coco.h5')
+
 modelo = modellib.MaskRCNN(mode="inference", config=InferenceConfig(), model_dir=caminho_Logs)
 
-caminho_pesos = caminho_pesos_coco
 if not os.path.exists(caminho_pesos):
      utils.download_trained_weights(caminho_pesos)
 modelo.load_weights(caminho_pesos, by_name=True)
 
-imagem = skimage.io.imread(arquivo_imagem)
-r = modelo.detect([imagem], verbose = 0)[0]
+if __name__ == '__main__':
+    import argparse
 
-for classe in r['class_ids']:
-    if((classe != 16) and (classe != 17)):
-        i = np.where(r['class_ids'] == classe)[0]
-        r['class_ids'] = np.delete(r['class_ids'], i, 0)
-        r['masks'] = np.delete(r['masks'], i, 2)
-        r['rois'] = np.delete(r['rois'], i, 0)
-        r['scores'] = np.delete(r['scores'], i, 0)
-
-for i in range(len(r['class_ids'])):
-    r['class_ids'][i] -= 15
-
-nome_arquivo = '.'.join(arquivo_imagem.split('.')[:-1])
-visualize.display_instances(imagem, r['rois'], r['masks'], r['class_ids'], 
-                            ['BG', 'Gato', 'Cachorro'], r['scores'],
-                            nome_arquivo = nome_arquivo)
-
-print("Arquivo salvo em " + nome_arquivo + "_detectado.png")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--imagem', required=True,
+                        metavar="caminho ou URL para uma imagem",
+                        help='Imagem para testar e aplicar a detecção')
+    
+    args = parser.parse_args()
+    
+    print('Detectando em ' + args.imagem + '...')
+    detectar_classes(modelo, args.imagem)
